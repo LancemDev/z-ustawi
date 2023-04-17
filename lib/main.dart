19a3bf935b987acbca0507f628cfd868db501aa7
LancemDev <munyaolance1@gmail.com>
2023-04-17 22:07:29 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,177 @@
+//Starting from Scratch, we will create a new project
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _widgets = [
+    const HomeWidget(),
+    const ProfileWidget(),
+    const SettingsWidget(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: _widgets[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+    );
+  }
+}
+
+//Home Widget
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Widget'),
+    );
+  }
+}
+
+//Profile Widget
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile Widget'),
+    );
+  }
+}
+
+//Settings Widget
+class SettingsWidget extends StatefulWidget {
+  const SettingsWidget({Key? key}) : super(key: key);
+@override
+  // ignore: library_private_types_in_public_api
+  _SettingsWidgetState createState() => _SettingsWidgetState();
+}
+
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  bool _notificationEnabled = true;
+  int _selectedThemeIndex = 0;
+  double _fontScale = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Notifications',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            SwitchListTile(
+              title: const Text('Enable Notifications'),
+              value: _notificationEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _notificationEnabled = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Appearance',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            ListTile(
+              title: const Text('Theme'),
+              trailing: DropdownButton<int>(
+                value: _selectedThemeIndex,
+                items: const [
+                  DropdownMenuItem<int>(
+                    value: 0,
+                    child: Text('Light'),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 1,
+                    child: Text('Dark'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedThemeIndex = value!;
+                  });
+                },
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Accessibility',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            ListTile(
+              title: const Text('Font Scale'),
+              trailing: Slider(
+                value: _fontScale,
+                min: 0.5,
+                max: 2.0,
+                divisions: 6,
+                onChanged: (value) {
+                  setState(() {
+                    _fontScale = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
